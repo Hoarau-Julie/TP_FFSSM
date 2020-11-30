3084a5ebd32d117b358e223527d46bf974641f35
Hoarau Julie <Hoarau Julie@192.168.1.21>
2020-11-30 11:24:57 +0100

    Tp FFSSM

--- src/main/java/FFSSM/Plongeur.java
+++ src/main/java/FFSSM/Plongeur.java
@@ -1,5 +1,49 @@
 package FFSSM;
 
-public class Plongeur {
-	
+import java.time.LocalDate;
+
+public class Plongeur extends Personne {
+    
+    public int niveau;
+    public Licence licence;
+    public Club club;
+    public GroupeSanguin groupeSanguin;
+    
+    public Plongeur(String numeroINSEE, String nom, String prenom, String adresse, String telephone, LocalDate naissance, GroupeSanguin groupeSanguin){
+        super(numeroINSEE, nom, prenom, adresse, telephone, naissance);
+        this.groupeSanguin = groupeSanguin;
+    }
+    
+    public int getNiveau(){
+        return niveau;
+    }
+    
+    public void setNiveau(int niveau){
+        this.niveau = niveau;
+    }
+    
+    public Licence getLicence(){
+        return licence;
+    }
+    
+    public Club getClub(){
+        return club;
+    }
+    
+    public void setClub(Club club){
+        this.club = club;
+    }
+    
+    public GroupeSanguin getGroupe(){
+        return groupeSanguin;
+    }
+    
+    public void ajouteLicence(String numero, LocalDate delivrance)throws Exception {
+        if(this.club == null){
+            throw new Exception("Ce plongeur doit être rattaché à un club pour obtenir une licence");
+        }
+        else {
+            licence = new Licence(this, numero, delivrance,club);
+        }
+    }
 }
