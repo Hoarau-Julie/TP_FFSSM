17ce9cc64e9c1ccee1a683be87607ab37a6f395a
Hoarau Julie <Hoarau Julie@192.168.1.21>
2020-11-30 11:31:47 +0100

    Tp FFSSM

--- src/main/java/FFSSM/Plongee.java
+++ src/main/java/FFSSM/Plongee.java
@@ -4,22 +4,22 @@
 package FFSSM;
 
 import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.LinkedList;
 
 public class Plongee {
 
 	public Site lieu;
-
 	public Moniteur chefDePalanquee;
-
 	public LocalDate date;
-
 	public int profondeur;
-
 	public int duree;
+        public LinkedList<Plongeur> participants = new LinkedList<>();
 
-	public Plongee(Site lieu, Moniteur chefDePalanquee, LocalDate date, int profondeur, int duree) {
+	public Plongee(Moniteur chef){
+            this.chefDePalanquee = chef;
+        }
+        
+        public Plongee(Site lieu, Moniteur chefDePalanquee, LocalDate date, int profondeur, int duree) {
 		this.lieu = lieu;
 		this.chefDePalanquee = chefDePalanquee;
 		this.date = date;
@@ -27,14 +27,21 @@ public class Plongee {
 		this.duree = duree;
 	}
 
-	public void ajouteParticipant(Plongeur participant) {
-		// TODO: Implémenter cette méthode
-		throw new UnsupportedOperationException("Pas encore implémenté");
+	public void ajouteParticipant(Plongeur participant) throws Exception 
+        {
+                if(participants.contains(participant)){
+                    throw new Exception("Ce plongeur a déjà été inscrit pour cette plongée");
+                }    
+                this.participants.add(participant);
 	}
 
 	public LocalDate getDate() {
 		return date;
 	}
+        
+        public void setDate(LocalDate date){
+            this.date = date;
+        }
 
 	/**
 	 * Détermine si la plongée est conforme. 
@@ -42,9 +49,34 @@ public class Plongee {
 	 * licence valide à la date de la plongée
 	 * @return vrai si la plongée est conforme
 	 */
-	public boolean estConforme() {
-		// TODO: Implémenter cette méthode
-		throw new UnsupportedOperationException("Pas encore implémenté");
-	}
+	public boolean estConforme()
+        {
+            //si le chef de palanquee n'a pas de licence
+            if(this.chefDePalanquee.getLicence() == null)
+            {
+                return false;
+            }
+            //si le chef de palanquee a une licence expirée
+            if(!this.chefDePalanquee.getLicence().estValide(this.date))
+            {
+                return false;
+            }
+            //si l'un des participants n'a pas de licence ou une licence expirée
+            if(!this.participants.isEmpty())
+            {
+                for(int i=0; i < participants.size(); i++)
+                {
+                    if(participants.get(i).getLicence() == null)
+                    {
+                        return false;
+                    }
+                    else if(!participants.get(i).getLicence().estValide(this.date))
+                    {
+                        return false;
+                    }
+                }
+            }
+                return true;
+        }
 
 }
