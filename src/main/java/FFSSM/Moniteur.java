9cfeb4a00477428dad3dd682596bc7378396dac5
Hoarau Julie <Hoarau Julie@192.168.1.21>
2020-11-30 11:25:22 +0100

    Tp FFSSM

--- src/main/java/FFSSM/Moniteur.java
+++ src/main/java/FFSSM/Moniteur.java
@@ -4,15 +4,17 @@
 package FFSSM;
 
 import java.time.LocalDate;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 
-public class Moniteur extends Personne {
+public class Moniteur extends Plongeur {
 
     public int numeroDiplome;
+    public LinkedList<Embauche> emplois;
 
-    public Moniteur(String numeroINSEE, String nom, String prenom, String adresse, String telephone, LocalDate naissance, int numeroDiplome) {
-        super(numeroINSEE, nom, prenom, adresse, telephone, naissance);
+    public Moniteur(String numeroINSEE, String nom, String prenom, String adresse, String telephone, LocalDate naissance, GroupeSanguin groupeSanguin, int numeroDiplome) {
+        super(numeroINSEE, nom, prenom, adresse, telephone, naissance, groupeSanguin);
         this.numeroDiplome = numeroDiplome;
     }
 
@@ -21,24 +23,45 @@ public class Moniteur extends Personne {
      * ce moniteur n'a pas d'employeur.
      * @return l'employeur actuel de ce moniteur sous la forme d'un Optional
      */
-    public Optional<Club> employeurActuel() {
-         // TODO: Implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+    public Optional<Club> employeurActuel() 
+    {
+        
+        for(int i = emplois.size()-1; i>0; i--){
+            if(this.emplois.get(i).getDebut().isBefore(LocalDate.now()))
+            {
+                if(!this.emplois.get(i).estTerminee())
+                {
+                    return Optional.of(this.emplois.get(i).getEmployeur());
+                }
+            }
+        }
+        return Optional.empty();
     }
     
     /**
      * Enregistrer une nouvelle embauche pour cet employeur
      * @param employeur le club employeur
      * @param debutNouvelle la date de début de l'embauche
+     * 
      */
-    public void nouvelleEmbauche(Club employeur, LocalDate debutNouvelle) {   
-         // TODO: Implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");	    
+    public void nouvelleEmbauche(Club employeur, LocalDate debutNouvelle) throws Exception {  
+        
+        LocalDate finDerniereEmbauche = this.emplois.getLast().getFin();
+        Embauche nouvelleEmbauche = new Embauche(debutNouvelle, this, employeur);
+        if(finDerniereEmbauche == null){
+            throw new Exception("La dernière embauche de ce moniteur doit avoir une date de fin pour en créer une nouvelle");
+        }
+        else if(debutNouvelle.isAfter(finDerniereEmbauche)){
+            this.emplois.addLast(nouvelleEmbauche);
+        }
+        else {
+            throw new Exception("Une nouvelle embauche ne peut pas démarrer avant la fin de la précédente");
+        }
     }
 
     public List<Embauche> emplois() {
-         // TODO: Implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+        
+        return this.emplois;
     }
-
+    
 }
